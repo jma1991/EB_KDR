96afa34c41d3a31a8988b5a57a705bd44d8a725a
James Ashmore <jma1991@icloud.com>
2021-08-09 18:18:57 +0100

    Build

--- _workflowr.R
+++ _workflowr.R
@@ -8,8 +8,13 @@ system("git add analysis/02-reduced-dimensions.Rmd")
 system("git add analysis/03-clustering.Rmd")
 system("git add analysis/04-marker-detection.Rmd")
 system("git add analysis/signaling-analysis.Rmd")
+system("git add analysis/scenic-analysis.Rmd")
+system("git add analysis/trajectory-analysis.Rmd")
+system("git add analysis/pseudotime-analysis.Rmd")
 system("git add analysis/mouse-integration.Rmd")
-# system("git add analysis/interactive.Rmd")
+system("git add analysis/mouse-trajectory.Rmd")
+system("git add analysis/mouse-pseudotime.Rmd")
+system("git add analysis/interactive.Rmd")
 system("git commit -m 'Build'")
 
 # Step 2: Build HTML files
@@ -22,8 +27,13 @@ wflow_build("analysis/02-reduced-dimensions.Rmd")
 wflow_build("analysis/03-clustering.Rmd")
 wflow_build("analysis/04-marker-detection.Rmd")
 wflow_build("analysis/signaling-analysis.Rmd")
+wflow_build("analysis/scenic-analysis.Rmd")
+wflow_build("analysis/trajectory-analysis.Rmd")
+wflow_build("analysis/pseudotime-analysis.Rmd")
 wflow_build("analysis/mouse-integration.Rmd")
-# wflow_build("analysis/interactive.Rmd")
+wflow_build("analysis/mouse-trajectory.Rmd")
+wflow_build("analysis/mouse-pseudotime.Rmd")
+wflow_build("analysis/interactive.Rmd")
 
 # Step 3: Add HTML files
 
@@ -35,8 +45,13 @@ system("git add docs/02-reduced-dimensions.html")
 system("git add docs/03-clustering.html")
 system("git add docs/04-marker-detection.html")
 system("git add docs/signaling-analysis.html")
+system("git add docs/scenic-analysis.html")
+system("git add docs/trajectory-analysis.html")
+system("git add docs/pseudotime-analysis.html")
 system("git add docs/mouse-integration.html")
-# system("git add docs/interactive.html")
+system("git add docs/mouse-trajectory.html")
+system("git add docs/mouse-pseudotime.html")
+system("git add docs/interactive.html")
 
 # Step 4: Add PNG files
 
@@ -45,7 +60,12 @@ system("git add docs/figure/02-reduced-dimensions.Rmd")
 system("git add docs/figure/03-clustering.Rmd")
 system("git add docs/figure/04-marker-detection.Rmd")
 system("git add docs/figure/signaling-analysis.Rmd")
+system("git add docs/figure/scenic-analysis.Rmd")
+system("git add docs/figure/trajectory-analysis.Rmd")
+system("git add docs/figure/pseudotime-analysis.Rmd")
 system("git add docs/figure/mouse-integration.Rmd")
+system("git add docs/figure/mouse-trajectory.Rmd")
+system("git add docs/figure/mouse-pseudotime.Rmd")
 
 # Step 5: Add site files
 
